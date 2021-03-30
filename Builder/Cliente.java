0e30d9b65f3bbacbe1f80852ab0ff4777e574e38
loiola0 <victorloiola@alu.ufc.br>
2021-03-30 14:38:20 -0300

    primeira parte feita

---
+++ Builder/Cliente.java
@@ -0,0 +1,11 @@
+package org.designpatterns;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		
+		CupomFiscal cmpf = new CupomFiscal.CupomFiscalBuilder("Loja de Fachada","1234934/8").build();
+		
+	}
+
+}
--- Builder/CupomFiscal.java
+++ Builder/CupomFiscal.java
@@ -1,10 +1,72 @@
 package org.designpatterns;
 
 public class CupomFiscal {
-	String nomeLoja;
-	String cnpj;
-	String cpfConsumidor;
-	String cnpjConsumidor;
-	double dinheiroRecebido;
-	Item listaItem;
+	//mandatory attributes
+	private String nomeLoja;
+	private String cnpj;
+	
+	//optional attributes
+	private String cpfConsumidor;
+	private String cnpjConsumidor;
+	private double dinheiroRecebido;
+	private Item listaItem;
+	
+	//constructor
+	private CupomFiscal(CupomFiscalBuilder builder) {
+		this.nomeLoja = builder.nomeLoja;
+		this.cnpj = builder.cnpj;
+		this.cnpjConsumidor = builder.cnpjConsumidor;
+		this.cpfConsumidor = builder.cpfConsumidor;
+		this.dinheiroRecebido = builder.dinheiroRecebido;
+		this.listaItem = builder.listaItem;
+	}
+	
+	
+	public static class CupomFiscalBuilder{
+		
+		//mandatory attributes
+		private String nomeLoja;
+		private String cnpj;
+		
+		//optional attributes
+		private String cpfConsumidor;
+		private String cnpjConsumidor;
+		private double dinheiroRecebido;
+		private Item listaItem;
+		
+		
+		public CupomFiscalBuilder(String nomeLoja, String cnpj) {
+			this.nomeLoja = nomeLoja;
+			this.cnpj = cnpj;
+		}
+		
+		public CupomFiscalBuilder cpfConsumidor(String cpfConsumidor) {
+			this.cpfConsumidor = cpfConsumidor;
+			return this;
+		}
+		
+		public CupomFiscalBuilder cnpjConsumidor(String cnpjConsumidor) {
+			this.cnpjConsumidor = cnpjConsumidor;
+			return this;
+		}
+		
+		public CupomFiscalBuilder dinheiroRecebido(double dinheiroRecebido) {
+			this.dinheiroRecebido = dinheiroRecebido;
+			return this;
+		}
+		
+		public CupomFiscalBuilder listaItem(Item listaItem) {
+			this.listaItem = listaItem;
+			return this;
+		}
+		
+		public CupomFiscal build() {
+			return new CupomFiscal(this);
+		}
+		
+	}
+	
+	
 }
+
+
